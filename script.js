1d173f0c67e8754d47f0177c974972e0f094a9f4
anujy <anuj.y@hotmail.com>
2022-06-30 16:04:37 -0400

    basic working operations and equal button, need to fix all exceptions and mathematical errors

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const addButton = document.getElementById('add');
-const subtractButton = document.getElementById('subtract');
-const multiplyButton = document.getElementById('multiply');
-const divideButton = document.getElementById('divide');
+const numberBttns = Array.from(document.getElementsByClassName('number'));
+const operationBttns = Array.from(document.getElementsByClassName('operation'));
 
-const equal = document.getElementById('equal');
-const clear = document.getElementById('clear');
-const display = document.getElementById('display');
+const equalBttn = document.getElementById('equal');
+const clearBttn = document.getElementById('clear');
+const memory = document.getElementById('memory');
+const input = document.getElementById('input');
+
+let firstOperand = '';
+let secondOperand = '';
+let currentOperation = null;
 
 function add(a, b) {
     return a+b;
@@ -24,5 +27,54 @@ function divide(a, b) {
 }
 
 function operate(a, b, operation) {
-    return operation(a, b);
+    switch(operation) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case 'x':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);        
+    }
+}
+
+numberBttns.forEach(bttn => bttn.addEventListener('click', () => appendNumber(bttn.textContent)));
+operationBttns.forEach(bttn => bttn.addEventListener('click', () => storeOperation(bttn.textContent)));
+clearBttn.addEventListener('click', () => clearDisplay());
+equalBttn.addEventListener('click', () => {
+    evaluate();
+});
+
+
+function appendNumber(num) {
+    input.textContent += num;
+}
+
+
+function storeOperation(operation) {
+    if (currentOperation != null) {
+        evaluate();
+    }
+    firstOperand = input.textContent;
+    currentOperation = operation;
+    memory.textContent = `${input.textContent} ${operation}`;
+    input.textContent = '';
+}
+
+function evaluate () {
+    if(currentOperation == null) return
+    secondOperand = input.textContent;
+    input.textContent = operate(parseInt(firstOperand), parseInt(secondOperand), currentOperation);
+    memory.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+    currentOperation = null;    
+}
+
+
+function clearDisplay() {
+    input.textContent = '';
+    memory.textContent = '';
+    firstOperand = '';
+    secondOperand = '';
+    currentOperation = null;
 }
